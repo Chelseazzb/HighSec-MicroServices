4c82705bfe8a0e5db482210dbad4649d045a5ccb
Kevin <1147623296@qq.com>
2020-07-14 10:57:36 +0800

    为OpenFeign客户端设置了FULL级别的debug日志

---
+++ springcloud-consumer-feignclient80/src/main/java/highsec/microservices/springcloud/config/FeignConfig.java
@@ -0,0 +1,20 @@
+package highsec.microservices.springcloud.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName : FeignConfig
+ * @Description : ToDo
+ * @Modified_by :
+ * @Version : 1.0
+ **/
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
